e67b7aa82543e447efeb46bcd551cb551f656337
Abhishek <vishabhi555@gmail.com>
2023-07-28 15:59:24 +0530

    done

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,36 +1,23 @@
 import jwt from "jsonwebtoken";
 import userModel from "../model/user.model";
+import { catchAsync } from "../utils/catchAsync";
 
-export const protect = async (req,res,next)=>{
-    try {
-        const {authorization} = req.headers;
-        if(!authorization || !authorization.startsWith("Bearer")){
-            throw new Error("please login to access")
-        }
-        const token = jwt.verify(authorization.split(" ")[1],process.env.SECRET_WEB_TOKEN);
-        const user = await userModel.findById(token.id);
-        if(!user) throw new Error("user doesn't exits");
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.status(400).json({
-            status:"failed",
-            message: error.message
-        })
+export const protect = catchAsync(async (req,res,next)=>{
+    const {authorization} = req.headers;
+    if(!authorization || !authorization.startsWith("Bearer")){
+        throw new Error("please login to access")
     }
-}
+    const token = jwt.verify(authorization.split(" ")[1],process.env.SECRET_WEB_TOKEN);
+    const user = await userModel.findById(token.id);
+    if(!user) throw new Error("user doesn't exits");
+    req.user = user;
+    next();
+})
 
-export const authorized = async (req,res,next)=>{
-    try {
-        const user = req.user;
-        if(user.role !== "admin"){
-            throw new Error("this user has no Rights")
-        }
-        next();
-    } catch (error) {
-        return res.status(400).json({
-            status:"failed",
-            message: error.message
-        })
+export const authorized = catchAsync(async (req,res,next)=>{
+    const user = req.user;
+    if(user.role !== "admin"){
+        throw new Error("this user has no Rights")
     }
-}
+    next();
+})
